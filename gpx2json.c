3f1f6a015c6f90c80299c853a89a3bd04451bb34
Deuter <wojciechbernat95@gmail.com>
2020-08-15 15:10:00 +0200

    Added "read structure" Implemented memory allocation for its and pointer validation.

--- gpx2json.c
+++ gpx2json.c
@@ -2,22 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define GPX_PARAM_SIZE 8
+#define GPX_SOURCE_SIZE 64
+#define GPX_ARRAY_SIZE 16
+
 #ERROR_DATA 0
+#ERROR_MEMORY ERROR_DATA	//code for memory allocation
 
-
-//Elevation 
-//latitude
-//longitude
-//time
 
 typedef struct gpxParamtersStruct
 {
-	char gpxEncoding[8];
-	char gpxVer[8];
-	char gpxSource[64];
+	char gpxEncoding[GPX_PARAM_SIZE];
+	char gpxVer[GPX_PARAM_SIZE];
+	char gpxSource[GPX_SOURCE_SIZE];
 	
 	int readLines;
 };
+
+typedef struct gpxReadStruct
+{
+	char gpxLatitude[GPX_ARRAY_SIZE];
+	char gpxLongitude[GPX_ARRAY_SIZE];
+	char gpxElevation[GPX_ARRAY_SIZE];
+	char gpxData[GPX_ARRAY_SIZE];
+	char gpxTime[GPX_ARRAY_SIZE];
+
+};
+
+
+
+
 
 
 int int main(int argc, char const *argv[])
@@ -29,10 +43,35 @@ int int main(int argc, char const *argv[])
 	}
 		
 
-	/* Create structure and pointer to its */
+	/* Create structures and pointers to thier */
 	gpxParamtersStruct sGpxParameters, *psGpxParameters = NULL;
+	gpxReadStruct sGpxRead, *psGpxRead = NULL;
+
 	/* Allocation memmory for structures */
-	psGpxParameters = malloc(sizeof(sGpxParameters)); 
+	psGpxParameters = malloc(sizeof(sGpxParameters));
+	psGpxRead = malloc(sizeof(sGpxRead));
+
+	/* Check memory allocations */
+	if (psGpxParameters == NULL)
+	{
+		fprintf(stderr, "Error. Didn't allocate memory for parameters's structure\n");
+		return ERROR_MEMORY;
+	}
+	else 
+	{
+		fprintf(stderr, "Created .gpx paramters's structure.\n" );
+	}
+
+	if (psGpxRead == NULL)
+	{
+		fprintf(stderr, "Error. Didn't allocate memory for read's structure\n");
+		return ERROR_MEMORY;
+	}
+	else 
+	{
+		fprintf(stderr, "Created .gpx read's structure.\n" );
+	}
+
 
 
 	FILE *inputFile;
