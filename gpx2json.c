05bc5c134ecb45e1817f87c899d251bbeff4d09f
Deuter <wojciechbernat95@gmail.com>
2020-08-17 14:06:06 +0200

    Added struct memoryGuard and implemented its body and init functions.

--- gpx2json.c
+++ gpx2json.c
@@ -9,29 +9,27 @@
 
 #define CORRECT_EXECUTE 1
 #define ERROR_DATA 0
-#define ERROR_MEMORY ERROR_DATA			//code for memory allocation
-#define ERROR_NULL_POINTER ERROR_DATA	//code for null pointer detection
-
+#define ERROR_MEMORY ERROR_DATA		  //code for memory allocation
+#define ERROR_NULL_POINTER ERROR_DATA //code for null pointer detection
 
 static unsigned int allocateMemCounter = 0;
 static unsigned int deallocateMemCounter = 0;
 
 void *allocateMemory(size_t size);
 void deallocateMemory(void *pMemoryHandler);
-int checkMemoryLeak(void);
-
+int memoryLeak(void);
 
 /** Structers definitons */
-typedef struct 
+typedef struct
 {
 	char gpxEncoding[GPX_PARAM_SIZE];
 	char gpxVer[GPX_PARAM_SIZE];
 	char gpxSource[GPX_SOURCE_SIZE];
-	
+
 	int readLines;
 } gpxParamtersStruct;
 
-typedef struct 
+typedef struct
 {
 	char gpxLatitude[GPX_ARRAY_SIZE];
 	char gpxLongitude[GPX_ARRAY_SIZE];
@@ -41,21 +39,32 @@ typedef struct
 
 } gpxReadStruct;
 
+typedef struct
+{
+	static unsigned int allocateMemoryCounter;
+	static unsigned int deallocateMemoryCounter;
+
+	unsigned int *pAllocMemCounter;
+	unsigned int *pDeallocMemCounter;
+
+	void *(*pAllocteMemory)(size_t);
+	void (*pDeallocateMemory)(void *);
+	int (*pMemoryLeak)(void);
+
+} memoryGuardStruct;
+
 /** Function prototypes */
 int valiateMemoryPointer(void *pointer);
-
-
-
-
+memoryGuardStruct *initMemoryGuard(void);
 
 int main(int argc, char const *argv[])
 {
 
-	if((argv == NULL) || argc == 0 ) {
-		fprintf(stderr ,"\nInvalid arguments.\n");
-		return ERROR_DATA ;
+	if ((argv == NULL) || argc == 0)
+	{
+		fprintf(stderr, "\nInvalid arguments.\n");
+		return ERROR_DATA;
 	}
-		
 
 	/* Create structures and pointers to thier */
 	gpxParamtersStruct sGpxParameters, *psGpxParameters = NULL;
@@ -66,18 +75,16 @@ int main(int argc, char const *argv[])
 	psGpxRead = malloc(sizeof(sGpxRead));
 
 	/* Check memory allocations */
-	if (!((valiateMemoryPointer((void *) psGpxParameters))))
+	if (!((valiateMemoryPointer((void *)psGpxParameters))))
 	{
 		fprintf(stderr, "ERROR_NULL_POINTER: psGpxParameters\n");
 		return ERROR_NULL_POINTER;
 	}
-	if(!(valiateMemoryPointer((void *)psGpxRead)))
-		{
+	if (!(valiateMemoryPointer((void *)psGpxRead)))
+	{
 		fprintf(stderr, "ERROR_NULL_POINTER: psGpxRead\n");
 		return ERROR_NULL_POINTER;
 	}
-
-
 
 	FILE *inputFile;
 	inputFile = fopen(argv[1], "r"); //first argument - gpx file to read gps position and time
@@ -86,36 +93,33 @@ int main(int argc, char const *argv[])
 		fprintf(stderr, "Cannot open inputFile\n");
 	}
 
-
-
-
 	/* Deallocation */
 	free(psGpxRead);
 	free(psGpxParameters);
 
 	/* code */
 	return 0;
-
 }
 
-
-int valiateMemoryPointer(void *pointer) {
+int valiateMemoryPointer(void *pointer)
+{
 	if (pointer == NULL)
 	{
 		fprintf(stderr, "Error: NULL_POINTER.\nDidn't allocate memory for read's structure\n");
 		return ERROR_NULL_POINTER;
 	}
-	else 
+	else
 	{
-		fprintf(stderr, "Allocate structure memory.\n" );
+		fprintf(stderr, "Allocate structure memory.\n");
 		return CORRECT_EXECUTE;
 	}
 }
 
-
-void *allocateMemory(size_t size) {
-	if(size < 1) {
-		fprintf(stderr, "ERROR_SIZE.\nSize passed to allocate: %ld\n", size );
+void *allocateMemory(size_t size)
+{
+	if (size < 1)
+	{
+		fprintf(stderr, "ERROR_SIZE.\nSize passed to allocate: %ld\n", size);
 		return NULL;
 	}
 
@@ -127,31 +131,56 @@ void *allocateMemory(size_t size) {
 		++allocateMemCounter;
 		return (pMemoryHandler);
 	}
-	else 
+	else
 	{
-		fprintf(stderr, "ERROR_NULL_POINTER\nDidn't allocate memory with %ld size.\n",size );
+		fprintf(stderr, "ERROR_NULL_POINTER\nDidn't allocate memory with %ld size.\n", size);
 	}
-
 }
 
-void deallocateMemory(void *pMemoryHandler) {
+void deallocateMemory(void *pMemoryHandler)
+{
 	if (pMemoryHandler != NULL)
 	{
 		free(pMemoryHandler);
 		++deallocateMemCounter;
 	}
-	else {
+	else
+	{
 		fprintf(stderr, "ERROR_NULL_POINTER\nCannot deallocate memory.\n");
 	}
 }
 
-int checkMemoryLeak(void) {
-	if(allocateMemCounter != deallocateMemCounter ) {
+int memoryLeak(void)
+{
+	if (allocateMemCounter != deallocateMemCounter)
+	{
 		fprintf(stderr, "ERROR_MEMORY\nDetect memory leak.\nDiffrence between allocante and deallocate: %u", (allocateMemCounter - deallocateMemCounter));
 		return ERROR_MEMORY;
 	}
-	else{
+	else
+	{
 		/* No leak memory detected*/
 		return CORRECT_EXECUTE;
 	}
+}
+
+memoryGuardStruct *initMemoryGuard(void)
+{
+	memoryGuardStruct *pMemoryGuard = NULL; //Create  pointerr
+	pMemoryGuard = malloc(sizeof(memoryGuardStruct));
+
+	if (pMemoryGuard == NULL)
+	{
+		fprintf(stderr, "ERROR_MEMORY\nMemory Guard has not been initalized.");
+		return NULL;
+	}
+
+	pMemoryGuard->allocateMemoryCounter = 0;
+	pMemoryGuard->deallocateMemoryCounter = 0;
+
+	pMemoryGuard->pAllocteMemory = allocateMemory;
+	pMemoryGuard->pDeallocateMemory = deallocateMemory;
+	pMemoryGuard->pMemoryLeak = memoryLeak;
+
+	return pMemoryGuard;
 }
