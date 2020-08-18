6f2771e090bc0333700967d2932bed9ef6d822c7
Deuter <wojciechbernat95@gmail.com>
2020-08-18 14:49:02 +0200

    Refactored findAuthor
    Also implemented generic static function.

--- extractData.c
+++ extractData.c
@@ -9,18 +9,25 @@ char type[] = "<type>";
 char trackingPoints[] = "<trkpt";
 char elevation[] = "<ele>";
 
-static size_t getSpan(char *s, char *start, char *end)
+static size_t getSpan(size_t size, char *start, char *end)
 {
-    return (strlen(s) - strlen(start) - strlen(end));
+    if (size > 0 && start != NULL && end != NULL)
+        return (size - strlen(start) - strlen(end));
+    else
+        return 0;
 }
 
-static char *getString(char *start, size_t size, size_t span, char *m)
+static char *getString(char *start, size_t size, size_t offset, size_t span)
 {
-    char *author;
-    author = malloc(size);
-    strncpy(author, start + sizeof(m) - 1, span);
+    if (start != NULL && size != 0 && offset != 0 && span != 0)
+    {
+        char *author;
+        author = malloc(size);
+        strncpy(author, start + offset - 1, span);
 
-    return author;
+        return author;
+    }
+    return NULL;
 }
 
 char *findAuthor(char *s, size_t size)
@@ -29,16 +36,9 @@ char *findAuthor(char *s, size_t size)
     begin = strstr(s, name);
     if (begin)
     {
-        end = strstr(s, end_name);
-        if (end)
-        {
-            size_t span = getSpan(s, name, end_name);
-            return getString(s, size, span, name);
-            //  strncpy(author, begin + sizeof(name) - 1, span);
-            //    printf("Author: %s", author);
-            //   printf("\n span: %lu\n", span);
-        }
-        return NULL;
+        size_t span = getSpan(size, name, end_name);
+        //printf("\n span: %lu\n", span);
+        return getString(begin, size, sizeof(name), span);
     }
     return NULL;
 }
